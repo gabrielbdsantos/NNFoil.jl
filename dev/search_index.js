29183f16d551e1b7cb1263bdb626b9d97cde27b8
Documenter.jl <documenter@juliadocs.github.io>
2025-09-12 22:21:23 +0000

    build based on 2f297e6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NNFoil","page":"Home","title":"NNFoil","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NNFoil.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
