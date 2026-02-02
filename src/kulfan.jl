4fd3283ac5f12d643cba0d20f215ea989c0e40b4
Gabriel B. Santos <9348147+gabrielbdsantos@users.noreply.github.com>
2026-02-02 15:06:04 -0300

    refactor: simplify the fitting algorithm

--- src/kulfan.jl
+++ src/kulfan.jl
@@ -68,17 +68,17 @@ Fits Kulfan (CST) parameters to airfoil coordinates.
 # Arguments
 
 - `coordinates::AbstractMatrix`: Airfoil coordinates with columns `[x, y]`.
-- `num_coefficients::Int=8`: Number of Bernstein polynomial coefficients per
-  surface.
-- `N1::Real=0.5`: Leading-edge exponent.
-- `N2::Real=1.0`: Trailing-edge exponent.
+- `num_coefficients_per_side::Int`: Number of Bernstein polynomial coefficients
+  per surface.
+- `N1::Real`: Leading-edge exponent.
+- `N2::Real`: Trailing-edge exponent.
 
 # Returns
 
 - `KulfanParameters`: Fitted upper and lower weights, leading-edge weight, and
-    trailing-edge thickness
+  trailing-edge thickness.
 """
-function KulfanParameters(coordinates; num_coefficients = 8, N1 = 0.5, N2 = 1.0)
+function KulfanParameters(coordinates; num_coefficients_per_side = 8, N1 = 0.5, N2 = 1.0)
     coords_upper, coords_lower = split_upper_lower_surfaces(coordinates)
 
     x_upper = @view coords_upper[:, 1]
@@ -89,42 +89,32 @@ function KulfanParameters(coordinates; num_coefficients = 8, N1 = 0.5, N2 = 1.0)
     trailing_edge_thickness = y_upper[1] - y_lower[end]
 
     fit = LsqFit.curve_fit(
-        (x, y) -> airfoil_cst(x, y, length(x_upper); N1, N2),
+        (x, p) -> airfoil_cst(x, p, length(x_upper); N1, N2),
         [x_upper; x_lower],
         [y_upper; y_lower],
-        [ones(2 * num_coefficients + 1); trailing_edge_thickness],
+        [ones(2 * num_coefficients_per_side + 1); trailing_edge_thickness],
         autodiff = :forwarddiff
     )
 
+    offset = 1
+
     if fit.param[end] < 0
+        offset = 0
+
         fit = LsqFit.curve_fit(
-            (x, y) -> airfoil_cst_zero_trailing_edge(x, y, length(x_upper); N1, N2),
+            (x, p) -> airfoil_cst(x, [p; 0], length(x_upper); N1, N2),
             [x_upper; x_lower],
             [y_upper; y_lower],
-            ones(2 * num_coefficients + 1),
+            ones(2 * num_coefficients_per_side + 1),
             autodiff = :forwarddiff
         )
-
-        upper_weights = fit.param[1:num_coefficients]
-        lower_weights = fit.param[(num_coefficients + 1):(end - 1)]
-        leading_edge_weight = fit.param[end]
-        trailing_edge_thickness = convert(typeof(leading_edge_weight), 0)
-
-        res = [fit.param; 0]
-    else
-        upper_weights = fit.param[1:num_coefficients]
-        lower_weights = fit.param[(num_coefficients + 1):(end - 2)]
-        leading_edge_weight = fit.param[end - 1]
-        trailing_edge_thickness = fit.param[end]
-
-        res = fit.param
     end
 
     return KulfanParameters(
-        upper_weights = res[1:num_coefficients],
-        lower_weights = res[(num_coefficients + 1):(2 * num_coefficients)],
-        leading_edge_weight = res[end - 1],
-        trailing_edge_thickness = res[end],
+        upper_weights = fit.param[1:num_coefficients_per_side],
+        lower_weights = fit.param[(num_coefficients_per_side + 1):(2 * num_coefficients_per_side)],
+        leading_edge_weight = fit.param[end - offset],
+        trailing_edge_thickness = offset * fit.param[end],
     )
 end
 
@@ -203,7 +193,7 @@ Evaluate the class function used in Kulfan’s parametrization.
 
 # Returns
 
-- Array of the same shape as `x`: Values of the class function
+- Array of the same shape as `x`: Values of the class function.
 """
 @inline function class_function(x, N1, N2)
     return @. (x^N1) * (1 - x)^N2
@@ -216,7 +206,7 @@ Kulfan shape function defined as a weighted sum of Bernstein polynomials.
 
 # Arguments
 
-- `x`: Nondimensional chordwise coordinates [0, 1].
+- `x`: Nondimensional chordwise coordinates.
 - `coefficients::AbstractVector`: Weights for the Bernstein polynomials.
 
 # Returns
@@ -240,17 +230,20 @@ CST (Class--Shape Transformation) airfoil parametrization.
 
 # Arguments
 
-- `x`: Nondimensional chordwise coordinates [0, 1]
-- `coefficients::AbstractVector`: Shape function weights
-- `leading_edge_weight::Real`: Leading-edge modification term
-- `trailing_edge_thickness::Real`: Trailing-edge thickness parameter
-- `N1::Real`: Leading-edge exponent (default: 0.5)
-- `N2::Real`: Trailing-edge exponent (default: 1.0)
+- `x`: Nondimensional chordwise coordinates [0, 1].
+- `coefficients::AbstractVector`: Shape function weights.
+- `leading_edge_weight::Real`: Leading-edge modification term.
+- `trailing_edge_thickness::Real`: Trailing-edge thickness parameter.
+
+# Keyword arguments
+
+- `N1::Real`: Leading-edge exponent (default: 0.5).
+- `N2::Real`: Trailing-edge exponent (default: 1.0).
 
 # Returns
 
 - Same shape as `x`: Airfoil surface coordinates defined by the CST
-  parametrization
+  parametrization.
 """
 function cst(
         x, coefficients, leading_edge_weight, trailing_edge_thickness;
@@ -268,20 +261,25 @@ end
 """
     airfoil_cst(x, parameters, x_split_id; N1=0.5, N2=1.0)
 
-Reconstruct an airfoil surface from Kulfan (CST) parameters.
+Generate the airfoil surface coordinates from Kulfan parameters.
 
 # Arguments
 
-- `x`: Vector of nondimensional chordwise coordinates (0–1)
-- `parameters::AbstractVector`: upper and lower weights, leading-edge weight,
-    trailing-edge thickness
-- `x_split_id::Int`: Index separating upper and lower surface coordinates
-- `N1::Real`: Leading-edge exponent (default: 0.5)
-- `N2::Real`: Trailing-edge exponent (default: 1.0)
+- `x::AbstractVector`: Nondimensional chordwise coordinates, following the
+  Selig ordering.
+- `parameters::AbstractVector`: Vector containing the CST parameters: upper and
+  lower weights, leading-edge weight, and trailing-edge thickness.
+- `x_split_id::Int`: Index separating the upper and lower surface coordinates
+  in `x`.
+
+# Keyword arguments
+
+- `N1::Real`: Leading-edge exponent.
+- `N2::Real`: Trailing-edge exponent.
 
 # Returns
 
-- `Vector`: Airfoil surface y-coordinates corresponding to `x`
+- `Vector{<:Real}`: Airfoil surface y-coordinates corresponding to `x`.
 """
 function airfoil_cst(x, parameters, x_split_id; N1 = 0.5, N2 = 1.0)
     weights..., leading_edge_weight, trailing_edge_gap = parameters
@@ -301,27 +299,4 @@ function airfoil_cst(x, parameters, x_split_id; N1 = 0.5, N2 = 1.0)
     )
 
     return [reverse(y_upper); y_lower]
-end
-
-"""
-    airfoil_cst_zero_trailing_edge(x, parameters, x_split_id; N1=0.5, N2=1.0)
-
-Reconstruct an airfoil surface from Kulfan (CST) parameters with a zero
-trailing-edge gap.
-
-# Arguments
-
-- `x`: Vector of nondimensional chordwise coordinates (0–1)
-- `parameters::AbstractVector`: upper and lower weights, and leading-edge
-  weight
-- `x_split_id::Int`: Index separating upper and lower surface coordinates
-- `N1::Real`: Leading-edge exponent (default: 0.5)
-- `N2::Real`: Trailing-edge exponent (default: 1.0)
-
-# Returns
-
-- `Vector`: Airfoil surface y-coordinates corresponding to `x`
-"""
-function airfoil_cst_zero_trailing_edge(x, parameters, x_split_id; N1 = 0.5, N2 = 1.0)
-    return airfoil_cst(x, [parameters; 0], x_split_id; N1, N2)
 end
