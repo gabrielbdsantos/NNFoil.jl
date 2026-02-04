dbc65f55188d049b3bd6221ad51d8dd7e6e56162
Gabriel B. Santos <9348147+gabrielbdsantos@users.noreply.github.com>
2026-02-04 09:02:35 -0300

    fix: update `examples` to the latest API

--- examples/example01.jl
+++ examples/example01.jl
@@ -1,11 +1,16 @@
-import NNFoil
-import DelimitedFiles
+using NNFoil
+using DelimitedFiles
 
-coordinates = DelimitedFiles.readdlm(joinpath(
-    NNFoil.DATA_PATH, split("../test/airfoils/raw/naca0018.dat", "/")...))
-kulfan_parameters = NNFoil.KulfanParameters(NNFoil.normalize_coordinates!(coordinates))
-network_parameters = NNFoil.NeuralNetworkParameters(; model_size = :xlarge)
+coordinates = readdlm(
+    abspath(
+        joinpath(
+            NNFoil.DATA_PATH, splitpath("../test/airfoils/raw/naca0018.dat")...
+        )
+    )
+)
+kulfan_parameters = KulfanParameters(normalize_coordinates!(coordinates))
+network_parameters = NeuralNetworkParameters(; model_size = :xlarge)
 alpha = -180:180
-Reynolds = 5e6
+Reynolds = 5.0e6
 
-analysis = NNFoil.evaluate(network_parameters, kulfan_parameters, alpha, Reynolds)
+analysis = evaluate(network_parameters, kulfan_parameters, alpha, Reynolds)
