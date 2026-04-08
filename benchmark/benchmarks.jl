3ed5d3292b518961bcbc5ac8ecdfe3379f192ede
Gabriel B. Santos <9348147+gabrielbdsantos@users.noreply.github.com>
2026-04-08 19:43:10 -0300

    ci(bench): add PR benchmark workflow

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,183 @@
+using BenchmarkTools
+using NNFoil
+
+# NOTE: Benchmarks run against the 'xlarge' model to emulate a more realistic
+# scenario.
+const NETWORK_PARAMETERS = NNFoil.NeuralNetworkParameters(; model_size = :xsmall)
+const KULFAN_PARAMETERS = NNFoil.KulfanParameters(
+    upper_weights = collect(0.1:0.1:0.8),
+    lower_weights = collect(-0.05:-0.05:-0.4),
+    leading_edge_weight = 0.03,
+    trailing_edge_thickness = 0.01,
+)
+
+const ALPHA = collect(range(-8.0, 12.0; length = 64))
+const REYNOLDS = collect(range(3.0e6, 8.0e6; length = length(ALPHA)))
+const FEATURES = NNFoil.build_features(KULFAN_PARAMETERS, ALPHA, REYNOLDS)
+
+const SUITE = BenchmarkGroup()
+
+# End-to-end {{{
+# ----------------------------------------------------------------------
+SUITE["end_to_end"] = BenchmarkGroup()
+
+SUITE["end_to_end"]["evaluate_out_of_place"] = @benchmarkable(
+    NNFoil.evaluate(NETWORK_PARAMETERS, KULFAN_PARAMETERS, ALPHA, REYNOLDS),
+    evals = 1,
+)
+
+SUITE["end_to_end"]["evaluate_in_place"] = @benchmarkable(
+    NNFoil.evaluate!(cache),
+    setup = (
+        cache = NNFoil.NeuralNetworkCache(
+            NETWORK_PARAMETERS,
+            KULFAN_PARAMETERS,
+            ALPHA,
+            REYNOLDS,
+        )
+    ),
+    evals = 1,
+)
+# }}}
+# Features {{{
+# ----------------------------------------------------------------------
+SUITE["features"] = BenchmarkGroup()
+
+SUITE["features"]["build_features_vector_vector"] = @benchmarkable(
+    NNFoil.build_features(KULFAN_PARAMETERS, ALPHA, REYNOLDS),
+    evals = 1,
+)
+
+SUITE["features"]["build_features_scalar_scalar"] = @benchmarkable(
+    NNFoil.build_features(KULFAN_PARAMETERS, 2.5, 5.0e6),
+    evals = 1,
+)
+
+SUITE["features"]["update_features_vector_vector"] = @benchmarkable(
+    NNFoil.update_features!(cache, KULFAN_PARAMETERS, ALPHA, REYNOLDS),
+    setup = (cache = NNFoil.NeuralNetworkCache(NETWORK_PARAMETERS, copy(FEATURES))),
+    evals = 1,
+)
+
+SUITE["features"]["update_features_scalar_scalar"] = @benchmarkable(
+    NNFoil.update_features!(cache, KULFAN_PARAMETERS, 2.5, 5.0e6),
+    setup = (
+        x_scalar = reshape(NNFoil.build_features(KULFAN_PARAMETERS, 2.5, 5.0e6), :, 1);
+        cache = NNFoil.NeuralNetworkCache(NETWORK_PARAMETERS, x_scalar)
+    ),
+    evals = 1,
+)
+# }}}
+# Kernels {{{
+# ----------------------------------------------------------------------
+SUITE["kernels"] = BenchmarkGroup()
+
+SUITE["kernels"]["forward_out_of_place"] = @benchmarkable(
+    NNFoil.forward(NETWORK_PARAMETERS, FEATURES),
+    evals = 1,
+)
+
+SUITE["kernels"]["forward_in_place"] = @benchmarkable(
+    NNFoil.forward!(y, NETWORK_PARAMETERS, tmp),
+    setup = (
+        cache_tuple = NNFoil.allocate_forward_cache(NETWORK_PARAMETERS, copy(FEATURES));
+        y = cache_tuple[1];
+        tmp = cache_tuple[2]
+    ),
+    evals = 1,
+)
+
+SUITE["kernels"]["squared_mahalanobis_distance_out_of_place"] = @benchmarkable(
+    NNFoil.squared_mahalanobis_distance(NETWORK_PARAMETERS, FEATURES),
+    evals = 1,
+)
+
+SUITE["kernels"]["squared_mahalanobis_distance_in_place"] = @benchmarkable(
+    NNFoil.squared_mahalanobis_distance!(y, NETWORK_PARAMETERS, FEATURES, tmp1, tmp2),
+    setup = (
+        y = zeros(size(FEATURES, 2), 1);
+        tmp1 = similar(FEATURES);
+        tmp2 = similar(FEATURES)
+    ),
+    evals = 1,
+)
+
+SUITE["kernels"]["confidence_correction_cache_path"] = @benchmarkable(
+    NNFoil.confidence_correction!(y, FEATURES, cache),
+    setup = (
+        y = NNFoil.forward(NETWORK_PARAMETERS, FEATURES);
+        cache = NNFoil.NeuralNetworkCache(NETWORK_PARAMETERS, copy(FEATURES))
+    ),
+    evals = 1,
+)
+
+SUITE["kernels"]["decode_outputs!"] = @benchmarkable(
+    NNFoil.decode_outputs!(y),
+    setup = (y = NNFoil.forward(NETWORK_PARAMETERS, FEATURES)),
+    evals = 1,
+)
+
+SUITE["kernels"]["flip_inputs!"] = @benchmarkable(
+    NNFoil.flip_inputs!(x),
+    setup = (x = copy(FEATURES)),
+    evals = 1,
+)
+
+SUITE["kernels"]["flip_outputs!"] = @benchmarkable(
+    NNFoil.flip_outputs!(y, tmp),
+    setup = (
+        y = NNFoil.forward(NETWORK_PARAMETERS, FEATURES);
+        tmp = zeros(size(y, 2))
+    ),
+    evals = 1,
+)
+
+SUITE["kernels"]["fuse_predictions!"] = @benchmarkable(
+    NNFoil.fuse_predictions!(y, y_flipped),
+    setup = (
+        y = NNFoil.forward(NETWORK_PARAMETERS, FEATURES);
+        y_flipped = copy(y)
+    ),
+    evals = 1,
+)
+
+SUITE["kernels"]["pack_output"] = @benchmarkable(
+    NNFoil.pack_output(y),
+    setup = (y = NNFoil.forward(NETWORK_PARAMETERS, FEATURES)),
+    evals = 1,
+)
+
+SUITE["kernels"]["pack_output!"] = @benchmarkable(
+    NNFoil.pack_output!(output, y),
+    setup = (
+        y = NNFoil.forward(NETWORK_PARAMETERS, FEATURES);
+        output = NNFoil.NeuralNetworkOutput(
+            analysis_confidence = zeros(size(y, 2)),
+            CL = zeros(size(y, 2)),
+            CD = zeros(size(y, 2)),
+            CM = zeros(size(y, 2)),
+            Top_Xtr = zeros(size(y, 2)),
+            Bot_Xtr = zeros(size(y, 2)),
+        )
+    ),
+    evals = 1,
+)
+# }}}
+# Pipelines {{{
+# ----------------------------------------------------------------------
+SUITE["pipelines"] = BenchmarkGroup()
+
+SUITE["pipelines"]["postprocess_flip_fuse_decode"] = @benchmarkable(
+    begin
+        NNFoil.flip_outputs!(y_flipped, tmp)
+        NNFoil.fuse_predictions!(y, y_flipped)
+        NNFoil.decode_outputs!(y)
+    end,
+    setup = (
+        y = NNFoil.forward(NETWORK_PARAMETERS, FEATURES);
+        y_flipped = copy(y);
+        tmp = zeros(size(y, 2))
+    ),
+    evals = 1,
+)
+# }}}
